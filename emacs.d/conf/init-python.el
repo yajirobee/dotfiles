d3bfe72818ef437a2fe44808f5d395adce7a283d
keisuke <secret.heart.ks@gmail.com>
2012-12-06 11:15:01 +0900

    trivial change

--- emacs.d/conf/init-python.el
+++ emacs.d/conf/init-python.el
@@ -6,7 +6,8 @@
 
 (when (require 'highlight-indentation nil t)
   (setq py-indent-offset 4)
-  ;(add-hook 'python-mode-hook 'highlight-indentation-current-column-mode))
+  ;(add-hook 'python-mode-hook 'highlight-indentation-current-column-mode)
+  )
 
 (provide 'init-python)
 ;;; init-python.el ends here
