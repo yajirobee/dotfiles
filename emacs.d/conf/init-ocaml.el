640f64e6d6e7950c0b5a062ebc6000493668e96d
keisuke <secret.heart.ks@gmail.com>
2013-05-16 21:24:00 +0900

    trivial change

--- emacs.d/conf/init-ocaml.el
+++ emacs.d/conf/init-ocaml.el
@@ -1,12 +1,20 @@
 ;;; Ocaml
 
-(setq auto-mode-alist (cons '("\.ml\w?" . tuareg-mode) auto-mode-alist))
-(autoload 'tuareg-mode "tuareg" "Major mode for editing Caml code" t)
-(autoload 'camldebug "camldebug" "Run the Caml debugger" t)
+(when (require 'tuareg nil t)
+  ;;(autoload 'tuareg-mode "tuareg" "Major mode for editing Caml code" t)
+  (autoload 'camldebug "camldebug" "Run the Caml debugger" t)
+  (autoload 'tuareg-imenu-set-imenu "tuareg-imenu"
+    "Configuration of imenu for tuareg" t)
+  (add-hook 'tuareg-mode-hook 'tuareg-imenu-set-imenu)
 
-(if (and (boundp 'window-system) window-system)
-    (when (string-match "XEmacs" emacs-version)
+  (setq auto-mode-alist
+        (append '(("\\.ml[ily]?$" . tuareg-mode)
+                  ("\\.topml$" . tuareg-mode))
+                auto-mode-alist))
+
+  (if (and (boundp 'window-system) window-system)
+      (when (string-match "XEmacs" emacs-version)
         (if (not (and (boundp 'mule-x-win-initted) mule-x-win-initted))
             (require 'sym-lock))
-        (require 'font-lock)))
+        (require 'font-lock))))
 (provide 'init-ocaml)
