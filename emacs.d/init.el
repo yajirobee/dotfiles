93c79392400622afe681ed46d6f6b8e3859f0ac0
Keisuke Suzuki <secret.heart.ks@gmail.com>
2016-03-16 22:03:16 +0900

    fix elpa sites

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -322,11 +322,11 @@
 
 ;;; package.el
 (when (require 'package nil t)
-  ;; Add package repository Marmalade and ELPA which is managed by creater
+  (add-to-list 'package-archives
+               '("melpa" . "http://melpa.milkbox.net/packages/") t)
   (add-to-list 'package-archives
                '("marmalade" . "http://marmalade-repo.org/packages/"))
-  (add-to-list 'package-archives
-               '("ELPA" . "http://tromey.com/elpa/"))
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
   ;; setting load path and loading installed packages
   (package-initialize))
 
