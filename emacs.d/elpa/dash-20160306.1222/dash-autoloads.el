28f9f9b956c6612b2201c5cbfb951de1b59196fb
Keisuke Suzuki <secret.heart.ks@gmail.com>
2016-03-25 00:33:04 +0900

    install flycheck

---
+++ emacs.d/elpa/dash-20160306.1222/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("dash.el") (22260 1843 306370 620000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
