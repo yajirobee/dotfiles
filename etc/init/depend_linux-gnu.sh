c1554b6bd98ad076d566c20e660c7cd282a00317
keisuke suzuki <keisukes0714@gmail.com>
2023-05-23 22:55:08 +0900

    ruby dependencies

--- etc/init/depend_linux-gnu.sh
+++ etc/init/depend_linux-gnu.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # now debian flavor only
-
+apt-get update
 apt-get install --yes \
         xsel \
         zsh \
@@ -10,6 +10,18 @@ apt-get install --yes \
         python3-pip \
         ripgrep \
         openssl \
+        libssl-dev \
         libffi-dev \
-        libedit-dev \
-        libyaml-dev        
+        plibedit-dev \
+        libyaml-dev \
+        autoconf \
+        build-essential \
+        rustc \
+        libreadline6-dev \
+        zlib1g-dev \
+        libgmp-dev \
+        libncurses5-dev \
+        libgdbm6 \
+        libgdbm-dev \
+        libdb-dev \
+        uuid-dev
